f1f80bd5cee1b90126d64e5f2523f92ba410bf59
usagiramen <imweijian@gmail.com>
2019-08-23 10:13:12 +0800

    adding eigenpairs

--- simple_pca/src/main/scala/Main.scala
+++ simple_pca/src/main/scala/Main.scala
@@ -9,7 +9,5 @@ object Main extends App {
 
     val red = reduction.pca(matrix)
 
-    red.productIterator.foreach(println)
-
 }
 
--- simple_pca/src/main/scala/tools.scala
+++ simple_pca/src/main/scala/tools.scala
@@ -5,23 +5,24 @@ import breeze.linalg._
 
 class DimensionReduction {
     def covariance(data:DenseMatrix[Double]) : DenseMatrix[Double] = {
-        // reference: https://towardsdatascience.com/principal-component-analysis-your-tutorial-and-code-9719d3d3f376
+        /* 
+        Calculate matrix covariance.
+
+        reference: https://towardsdatascience.com/principal-component-analysis-your-tutorial-and-code-9719d3d3f376
+
+        */
 
         // calculate mean of data.
         val x_mean = mean(data.t(*, ::))
+
         // calculate dot product covariance.
         val covariance = (data.t(::, *) - x_mean) * (data.t(::, *) - x_mean).t
         
+        // divide all elements in matrix.
         covariance :/= (data.rows.toDouble - 1.0)
 
         // final result.
         covariance
-    }
-
-    def eigenvectors(cov:DenseMatrix[Double]) = {
-        val es = eig(cov)
-
-        (es.eigenvalues, es.eigenvectors)
     }
 
     def pca(x:DenseMatrix[Double]) = {
@@ -29,13 +30,26 @@ class DimensionReduction {
 
         // step 1: find the covariance matrix.
         val cov = covariance(x)
-
+        print(cov.cols)
         // step 2: calculate eigenvalues and eigenvectors.
-        val eigen = eigenvectors(cov)
+        val es = eig(cov)
+
+        val eigenvalues = es.eigenvalues
+                            .toArray
         
+        val eigenvectors = es.eigenvectors
+
+        val eigenpairs = eigenvalues.foreach(values => (1, values))
+        println(eigenpairs)
+        println(eigenvectors)
+        println(eigenvectors(0, ::))
+
+        // eigenvalues.foreach(v => println(v))
+        // println(es.eigenvectors)
+        // eigenvectors.foreach(v => println(v))
+
         // step 3: reorder eigenvalues by variance.
 
-        eigen
     }
 }
 
