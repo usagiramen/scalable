5717de06f91d8f2c36cfd3e3b198bc257836b3d5
usagiramen <imweijian@gmail.com>
2019-08-27 08:48:07 +0800

    add docstring

--- simple_pca/src/main/scala/tools.scala
+++ simple_pca/src/main/scala/tools.scala
@@ -4,12 +4,39 @@ import breeze.stats.mean
 import breeze.linalg._
 
 class DimensionReduction {
+    def pca(x:DenseMatrix[Double], threshold:Double): DenseMatrix[Double] = {
+        /** Process Principle Component Analysis.
+          * 
+          * This function automatically counts number of vectors
+          * to keep once the sum of eigenvalues (perc) reaches the
+          * threshold.
+          */
+
+        // step 1: find the covariance matrix.
+        val cov = covariance(x)
+
+        // step 2: get eigenpairs: an array of eigenvalues and
+        // eigenvectors tuple.
+        val (sum_values, pairs) = eigen_magic(cov)
+
+        // step 3: build projection matrix.
+        val vectors_to_keep = sum_values.indexWhere(_ >= threshold)
+        val projection = projection_matrix(pairs, vectors_to_keep, x.cols)
+        
+        // step 4: combine raw data with projection matrix.
+        val output = x * projection
+
+        output
+    }
+
     private def covariance(data:DenseMatrix[Double]) : DenseMatrix[Double] = {
-        /* 
-        Calculate matrix covariance.
-
-
-        */
+        /** Build matrix covariance.
+          * 
+          * Calculate the covariance between variance in a matrix using
+          * dot product. Formula reference: https://bit.ly/2z2DQpd
+          * 
+          * TODO: Add MinMax Scalar preprocess before  
+          */
 
         // calculate mean of data.
         val x_mean = mean(data.t(*, ::))
@@ -25,9 +52,14 @@ class DimensionReduction {
     }
 
     private def eigen_magic(cov:DenseMatrix[Double]) = {
+        /** Get array of eigenvalues and eigenvectors.
+          *
+          * This function receives covariance matrix and builds an array of
+          * eigenvalues and eigenvector tuples. It also sort the eigenvalues
+          * and project in a cumulative sum.
+          */
 
-
-        // get eigenvectors and eigenvalues.
+        // calculate eigenvectors and eigenvalues.
         val es = eig(cov)
         val evectors = es.eigenvectors
         val evalues = es.eigenvalues
@@ -39,7 +71,7 @@ class DimensionReduction {
             pairs = pairs :+ ((evalues(idx), evectors(::, idx)))
         }
  
-        // reorder eigenvalues by variance
+        // sort eigenvalues by variance in descending order
         // and calculate cumulative sum.
         val sorted = evalues.sorted(Ordering[Double].reverse).map{
             v => v / evalues.reduceLeft{_ + _}
@@ -50,7 +82,12 @@ class DimensionReduction {
     }
 
     private def projection_matrix(pairs:Array[(Double, DenseVector[Double])], vecs:Int, cols:Int) = {
-        
+        /** Build projection matrix.
+          * 
+          * It initalise the matrix with first vector, then adds the
+          * remaining vectors according to eigenvalues to keep.
+          */
+
         var projection = pairs(0)._2
                                  .asDenseMatrix
                                  .reshape(cols, 1)
@@ -66,36 +103,23 @@ class DimensionReduction {
         projection
     }
 
-    def pca(x:DenseMatrix[Double], threshold:Double): DenseMatrix[Double] = {
-
-
-        // step 1: find the covariance matrix.
-        val cov = covariance(x)
-
-        // step 2: get eigenpairs: an array of eigenvalues and
-        // eigenvectors tuple.
-        val (sum_values, pairs) = eigen_magic(cov)
-
-        // step 3: build projection matrix.
-        val vectors_to_keep = sum_values.indexWhere(_ >= threshold)
-        val projection = projection_matrix(pairs, vectors_to_keep, x.cols)
-        
-        // step 4: dot product raw data with projection matrix.
-        val output = x * projection
-
-        output
-    }
+    
 }
 
 class DataInput {
     def csv_to_matrix(filename:String) : DenseMatrix[Double] = {
-        // convert data from csv to 2d array.
+        /** Convert CSV file into DenseMatrix.
+         *  
+         * This function assumes the CSV only contains numbers.
+         * 
+         * TODO: add number validation and exclude non-number columns.
+         */
+
         val data = io.Source.fromFile(filename)
                             .getLines()
                             .drop(1)  // remove headers.
                             .map(_.split(",").map(_.trim.toDouble))
                             .toArray
-                            .dropRight(1)
 
         val matrix = DenseMatrix(data:_*)
 
